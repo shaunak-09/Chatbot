d406b692b3a11b9d2ac7ddeb5e08eb02ac346ffb
shaunak-09 <shaunakghosh09@kgpian.iitkgp.ac.in>
2023-06-08 16:40:11 +0530

    .

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://6481b659d095bf137212a7f6--stalwart-sfogliatella-ab9450.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
